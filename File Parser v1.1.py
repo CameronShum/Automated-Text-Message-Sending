7f1cb6789c3721f010a942c3e8e4f7cf377f2073
CameronShum <48306422+CameronShum@users.noreply.github.com>
2019-03-25 20:09:04 -0400

    Add files via upload

---
+++ File Parser v1.1.py
@@ -0,0 +1,14 @@
+fin = open('info.txt', 'r')
+lines = fin.readlines()
+
+def file_read(lookfor):
+    i = 0
+    num = ""
+    index = lines[1].find('"'+lookfor+'":"')+len(lookfor)+4
+    #finds the index of a string, adds the length of the string to get the index of the character that follows
+    while lines[1][index+i] != '"':
+        num += (lines[1][index+i])
+        #stores all values after within "" into a string
+        i+=1
+    return (float(num))
+    #returns a float for numerical operations
